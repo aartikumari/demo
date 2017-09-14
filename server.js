146aed7e919f4308b2a50ef1ff1bb2e7f9280bfa
aarti kumari <aarti.kumari@atos.net>
2017-09-14 07:48:13 -0400

    added example file

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 this is my first file
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 
 
 
 changes after stash
+=======
+=======
+>>>>>>> Stashed changes
+second change
+
+
+
+i m changeing somthing
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
